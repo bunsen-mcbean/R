d1cc04c47cf290016f6bbecc1f0f0b5ad1ee676f
bunsen-mcbean <58019620+bunsen-mcbean@users.noreply.github.com>
2023-05-15 19:25:22 +1000

    Update patch-framework.py

--- patch-framework.py
+++ patch-framework.py
@@ -121,6 +121,7 @@ libs.append(os.path.join(wd, "R.framework/Versions/4.1/Resources/bin/exec/R"))
 
 dependencies = extract_lib_dependencies(libs)
 dependencies.append('/usr/local/opt/openjpeg/lib/libopenjp2.7.dylib')  # sf package needs this
+dependencies.append('/opt/X11/lib/libXrender.1.dylib')  # gdtools package needs this
 
 new_libs = [ ]
 changes = [ ]
